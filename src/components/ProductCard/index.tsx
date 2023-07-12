a19040ce79ef0afa2c7cdf6f11b58e1847c64527
jvmakense <joaomakenze@gmail.com>
2023-07-12 07:52:58 -0300

    atualizacao

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,20 @@
+
+import  { Container, Name } from'./style.ts'
+
+
+
+
+function ProductCard() {
+
+    return (
+       <Container>
+        <Name>
+
+        </Name>
+        <Image src='' alt=''/>
+       </Container>
+    )
+        
+    
+}
+export default ProductCard
---
+++ src/components/ProductCard/style.ts
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+
+export const Name = styled.span`
+    font-size: 16px;
+    color: #5c5c5c;
+    margin-bottom: 16px;
+`
+
+export const Image = styled.img`
+    width: 216px;
+    height: 164px;
+    border-radius: 4px;
+    box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.25);
+
+
+`
